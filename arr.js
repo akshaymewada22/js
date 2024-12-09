171d2594039193d76000a3bd39636f3d8f62a419
akshaymewada22 <akshaymewada86@gmail.com>
2024-12-09 12:56:35 -0800

    first commit

---
+++ arr.js
@@ -0,0 +1,19 @@
+function arr1(){
+
+
+    let fruits=["apple","mango","banana"]
+  // let fruits1=[ "grapes",363,74]
+    // fruits.pop();
+
+    //  fruits.push("grapes")
+     fruits.shift();
+    //  fruits.unshift("grapes" );
+    //  console.log(fruits.slice(0,3))
+
+
+    console.log(fruits)
+      // fruits.splice(2,1,"grapes","rcb" ,"mi")
+      //    console.log(fruits .concat (fruits1))
+} 
+
+arr1();
---
+++ day.js
@@ -0,0 +1,175 @@
+
+
+ function run(){
+
+
+    // alert message ===============
+
+
+
+    // alert ( "hello i ma alert message")
+
+
+
+
+//   this is prompt =============
+
+
+    // prompt(" hello  i am propt mesage")
+// this is confirm============================================================================================
+
+
+    // confirm ( " hello confirm")
+    // let num1=1;
+    //   let num2=3;
+    // alert("this is sum"+  (num1+num2))
+
+// parseInt =========================================================================================================
+    // let num1= parseInt(prompt( "enter the number"));
+    // let num2= parseInt(prompt(" enter num"));
+
+    // alert( num1+num2);
+
+    // parseFloat=========================================================================================
+
+    // let num1= parseFloat(prompt( "enter the number"));
+    // let num2= parseFloat(prompt(" enter num"));
+
+    // alert( num1+num2); 
+
+// ======================================================================================================================
+//    let num=prompt("enter your age");
+
+//      if(  num>18){
+//          alert( "you can do" );
+//     } else{
+//         alert("no")
+//     }
+
+// ================================================================================================
+let age= Number(prompt("enter the age"))
+
+    alert (age)
+     age>=18?alert("eligihle"):alert("not eligihle")
+
+// let couse= Number( prompt( "confirm"))
+
+// couse=Number&alert("ok")
+// alert( "not ok")
+// ================================================================================================
+  
+    
+// let num=(prompt ("enter number"))
+
+// if( num%2==0){
+//     alert("even")
+
+//  }
+
+//  else{
+//     alert("odd")
+//  }
+
+
+// ======================================================================================
+    
+// let num=(prompt ("enter number"))
+
+// if( num>0){
+//     alert("pos")
+
+//  }
+
+//  else{
+//     alert("neg")
+//  }
+
+// q=3==============================================================================================
+ 
+// let price =prompt("enter item price")
+// let condis= confirm("u went dis")
+
+// if(condis){
+//     let condis=price-(price*0.10)
+//     alert(condis)
+// }
+
+//     else{
+//         alert("no dis")
+//     }
+
+// ====================================================================
+
+// let para=document.querySelector("#text");
+ 
+//  para.innerHTML=("hello ");
+//  para.style.color="red";
+//  para.style.fontSize="50px"
+//  para.style.width="1000px"
+
+        
+// let para=document.querySelector("#text");
+
+// if( para.style.display==="none"){
+//      para.style.display="block";
+// }
+//     else{
+//          para.style.display="none";
+//     }
+// let para=document.querySelector("#text");
+
+
+// para.style.backgroundColor="red";
+
+
+
+// let para=document.querySelector("#ptext");
+// para.innerHTML=("hello cybrom ");
+
+// let para=document.querySelector("#ptext");
+// para.style.backgroundColor="red";
+
+
+
+
+
+// run ()}
+
+// function run1(){
+
+    
+// // let para=document.querySelector("#ptext");
+// // para.style.backgroundColor="red";
+
+
+// // let para=document.querySelector("#ptext");
+
+
+
+// }
+
+
+// let count=0;
+// function run2(){
+
+//   count++;
+//     let num=document.querySelector("#text");
+
+//     num.innerHTML=count; 
+
+
+
+// }
+
+// function run3(){
+
+
+//     count--;
+    
+//     let num1=document.querySelector("#text3");
+//     num1.innerHTML = count;
+
+
+// }
+
+ }
